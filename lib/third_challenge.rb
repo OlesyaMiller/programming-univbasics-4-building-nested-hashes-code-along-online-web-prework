4aeec470043191d76bfdbf3a3e32debfdb452a18
OLESYA MILLER <olesyam.miller@gmail.com>
2019-11-23 06:48:04 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,7 +14,7 @@ def third_challenge
         #your key/value pairs here
 
       :hero => {
-        :mane => "Romeo",
+        :name => "Romeo",
         :age => "15",
         :status => "alive"
         #your key/value pairs here
