e26dd8f80e80493dd777bba50c8f42ad59aa39c2
OLESYA MILLER <olesyam.miller@gmail.com>
2019-11-23 06:47:20 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -6,13 +6,13 @@ def third_challenge
         :age => "53"
       },
         #your key/value pairs here
-        },
+
       :matriarch => {
         :name => "Lady Montague",
         :age => "54"
       },
         #your key/value pairs here
-        },
+
       :hero => {
         :mane => "Romeo",
         :age => "15",
